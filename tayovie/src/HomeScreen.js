3fec2f0e718b065a16dc8fdbe2327d604da2cd1f
Tamer Yazar <tamer.yazar@etts.com.tr>
2021-05-16 20:40:07 +0300

    Building tayovie architecture

---
+++ tayovie/src/HomeScreen.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './HomeScreen.css';
+
+function HomeScreen() {
+    return (
+        <div className ="homeScreen">
+            <h1>tayovie</h1>
+            
+            {/* NavBar */}
+            
+            {/* Banner */}
+            
+            {/* Row */}
+        
+        </div>
+    )
+}
+
+export default HomeScreen
---
+++ tayovie/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCSarqOQV1xNaaS7Nm2KXmi6lslCunbM30",
+    authDomain: "tayovie.firebaseapp.com",
+    projectId: "tayovie",
+    storageBucket: "tayovie.appspot.com",
+    messagingSenderId: "986201269956",
+    appId: "1:986201269956:web:6edada2aabd0f436204b91"
+  };
