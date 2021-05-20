0b11b68e4ee23f78c7bbb6ac8819a9153affac1e
Tamer Yazar <tamer.yazar@etts.com.tr>
2021-05-20 19:43:15 +0300

    Tayovie row part done- added a row section with banner field..

--- tayovie/src/HomeScreen.js
+++ tayovie/src/HomeScreen.js
@@ -2,15 +2,24 @@ import React from "react";
 import Banner from "./Banner";
 import "./HomeScreen.css";
 import Nav from "./Nav";
+import requests from "./Requests";
+import Row from "./Row";
 
 function HomeScreen() {
   return (
     <div className="homeScreen">
       <Nav />
-
       <Banner />
-
-      {/* Row */}
+      <Row
+        title="TAYOVIE ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      ​<Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
--- tayovie/src/Requests.js
+++ tayovie/src/Requests.js
@@ -2,7 +2,7 @@
 const API_KEY = "85f4cd06e9d8bc562c5c412a84e3b327";
 
 const requests = {
-  /*   ​fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`, */
+  
   fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
   fetchTopRated: `/discover/top_rated?api_key=${API_KEY}&language=en-US`,
   fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
